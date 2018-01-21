c0247afa40fca2db4f27e0536b2b344cd8dea6a2
aborruso <aborruso@gmail.com>
2018-01-21 17:17:19 +0100

    passaggio all'anagrafica "unapromessa"

--- script/everypolitcian.sh
+++ script/everypolitcian.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+<<comment1
+comment1
 # scarico il dataset di base di everypolitician
-curl -sL "https://cdn.rawgit.com/everypolitician/everypolitician-data/09f85d30811e3e5f7bd6227480f6dfb0b447b4f9/data/Italy/House/ep-popolo-v1.0.json" > ../data/everypoliticianItalia.json
+#curl -sL "https://cdn.rawgit.com/everypolitician/everypolitician-data/09f85d30811e3e5f7bd6227480f6dfb0b447b4f9/data/Italy/House/ep-popolo-v1.0.json" > ../data/everypoliticianItalia.json
 
 # estraggo info di base (ID WikiDATA, URL Immagine e nome, ecc.)
-< ../data/everypoliticianItalia.json jq '[.persons[] | {wikidata:.identifiers[]| select(.scheme | contains("wikidata"))|.identifier,immagine:.image,nome:.name,everypolitician_legacy:.identifiers[]| select(.scheme | contains("everypolitician_legacy"))|.identifier,wikipediaIT:.links[]| select(.note | contains("Wikipedia (it)"))|.url,id:.id}]' | tee ../data/everypoliticianItaliaPersons.json | in2csv -I -f json > ../data/everypoliticianItaliaPersons.csv
+#< ../data/everypoliticianItalia.json jq '[.persons[] | {wikidata:.identifiers[]| select(.scheme | contains("wikidata"))|.identifier,immagine:.image,nome:.name,everypolitician_legacy:.identifiers[]| select(.scheme | contains("everypolitician_legacy"))|.identifier,wikipediaIT:.links[]| select(.note | contains("Wikipedia (it)"))|.url,id:.id}]' | tee ../data/everypoliticianItaliaPersons.json | in2csv -I -f json > ../data/everypoliticianItaliaPersons.csv
 
 
 # estraggo info di base (ID WikiDATA, URL Immagine e nome, ecc.)
-< ../data/everypoliticianItalia.json jq '[.persons[] | {wikidata:.identifiers[]| select(.scheme | contains("wikidata"))|.identifier,immagine:.image,nome:.name,everypolitician_legacy:.identifiers[]| select(.scheme | contains("everypolitician_legacy"))|.identifier,wikipediaIT:.links[]| select(.note | contains("Wikipedia (it)"))|.url,id:.id,twitter:.contact_details[]? | select(.type | contains("twitter")) | .value}]' | in2csv -I -f json > ../data/everypoliticianItaliaPersonsTwitter.csv
+#< ../data/everypoliticianItalia.json jq '[.persons[] | {wikidata:.identifiers[]| select(.scheme | contains("wikidata"))|.identifier,immagine:.image,nome:.name,everypolitician_legacy:.identifiers[]| select(.scheme | contains("everypolitician_legacy"))|.identifier,wikipediaIT:.links[]| select(.note | contains("Wikipedia (it)"))|.url,id:.id,twitter:.contact_details[]? | select(.type | contains("twitter")) | .value}]' | in2csv -I -f json > ../data/everypoliticianItaliaPersonsTwitter.csv
 
-cat ../data/everypoliticianItaliaPersons.csv > ../tmp/everypoliticianItaliaPersons.csv
+#cat ../data/everypoliticianItaliaPersons.csv > ../tmp/everypoliticianItaliaPersons.csv
 
 # faccio il join con la tabella che contiene i datti sugli account twitter e creo CSV che li contiene
-csvsql -I --query "select A.*,B.twitter from everypoliticianItaliaPersons AS A LEFT JOIN everypoliticianItaliaPersonsTwitter AS B on A.id=B.id" ../tmp/everypoliticianItaliaPersons.csv ../data/everypoliticianItaliaPersonsTwitter.csv > ../data/everypoliticianItaliaPersons.csv
+#csvsql -I --query "select A.*,B.twitter from everypoliticianItaliaPersons AS A LEFT JOIN everypoliticianItaliaPersonsTwitter AS B on A.id=B.id" ../tmp/everypoliticianItaliaPersons.csv ../data/everypoliticianItaliaPersonsTwitter.csv > ../data/everypoliticianItaliaPersons.csv
 
-rm ../data/everypoliticianItaliaPersonsTwitter.csv
+#rm ../data/everypoliticianItaliaPersonsTwitter.csv
 
 # estraggo il JSON con i dati twitter
-csvjson -I ../data/everypoliticianItaliaPersons.csv | jq . > ../data/everypoliticianItaliaPersons.json
+#csvjson -I ../data/everypoliticianItaliaPersons.csv | jq . > ../data/everypoliticianItaliaPersons.json
+
+curl -sL "https://docs.google.com/spreadsheets/d/e/2PACX-1vTquc_cJduDFPPrXtZvS22SD0L_hy5mQaaOH2__QKF4Fi8Y-QcKNN8gXVUatOr-TPuTv6gTEZ0rB0W6/pub?gid=249352183&single=true&output=csv" > ../data/unapromessa_raw.csv
+
+< ../data/unapromessa_raw.csv csvgrep -c "immagine" -i -r "^$" > ../data/unapromessa.csv
+
+csvjson -I ../data/unapromessa.csv | jq . > ../data/unapromessa.json
